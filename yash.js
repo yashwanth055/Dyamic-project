e43a0d7a2ba8aad624e847a247a827d603fd544b
yashwanth055 <ykondaki@aum.edu>
2025-03-23 20:22:41 -0500

    Add files via upload

---
+++ yash.js
@@ -0,0 +1,12 @@
+let x = 10;
+console.log(x);
+let y = x + 5;
+console.log(y);
+let a = x * 4;
+console.log(a);
+let b = x - 4;
+console.log(b);
+let c = x / 3;
+console.log(c);
+let d = x / 5;
+console.log(d);
